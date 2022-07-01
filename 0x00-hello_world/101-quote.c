6d1b9119a4704d92732dbfe1efbc78a5653ed9e8
anointingjoe <josephodey.88@gmail.com>
2022-06-30 21:04:10 -0700

    Dora Korpar Quote 2015

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,12 @@
 *
 * Return: Always 1 (Success)
 */
-
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-fprintf(stderr, "and that piece of art is useful\" - \
+printf(stderr, "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n");
 return (1);
 }
