fdf9dbca18749cad11d6c2773b3924a3a539da67
anointingjoe <josephodey.88@gmail.com>
2022-07-04 01:21:03 -0700

    different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,31 +3,29 @@
 #include <stdio.h>
 
 /**
- *main - Task 1 print the last digit of the number stored in the variable n
+ *main - Task 11 program prints different combination of three digits.
  *
  *Return: 0
  **/
-
 int main(void)
 {
-int ch;
-int n;
-int m;
-for (ch = 48; ch <= 57; ch++)
+int i, j, k;
+int arr[3];
+putchar("Enter the first number : ");
+scanf("%d", &arr[0]);
+putchar("Enter the second number : ");
+scanf("%d", &arr[1]);
+putchar("Enter the third number : ");
+scanf("%d", &arr[2]);
+for (i = 0; i < 3; i++)
 {
-for (n = 49; n <= 57; n++)
+for (j = 0; j < 3; j++)
 {
-for (m = 50; m <= 57; m++)
+for (k = 0; k < 3; k++)
 {
-if (n > ch && m > n)
+if (i != j && j != k && k != i)
 {
-putchar(ch);
-putchar(n);
-putchar(m);
-if (ch != 55 || n != 56 || m != 57)
-{
-putchar(44);
-putchar(32);
+putchar("[%d %d %d]\n", arr[i], arr[j], arr[k]);
 }
 }
 }
