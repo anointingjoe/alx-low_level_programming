28aac7f7312ebb01a908f2bc3d93618f91b899d2
anointingjoe <josephodey.88@gmail.com>
2022-07-04 01:26:35 -0700

    combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,12 +11,6 @@ int main(void)
 {
 int i, j, k;
 int arr[3];
-putchar("Enter the first number : ");
-scanf("%d", &arr[0]);
-putchar("Enter the second number : ");
-scanf("%d", &arr[1]);
-putchar("Enter the third number : ");
-scanf("%d", &arr[2]);
 for (i = 0; i < 3; i++)
 {
 for (j = 0; j < 3; j++)
