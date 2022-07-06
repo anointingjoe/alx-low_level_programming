d50c98ac122187fc3a7cf5dc9ccbcd3cf4359bd8
anointingjoe <josephodey.88@gmail.com>
2022-07-06 08:59:30 -0700

    print small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (k = 'a'; k <= 'z'; k++)
+{
+_putchar(k);
+}
+_putchar('\n');
+}
