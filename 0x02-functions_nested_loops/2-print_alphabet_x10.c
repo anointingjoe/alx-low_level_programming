59b04899523b61d794ba886b5fcb7a3871eeb340
anointingjoe <josephodey.88@gmail.com>
2022-07-06 09:33:12 -0700

    10x abc

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
  * Return: 0
  **/
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char k;
 char l;
 
-for (l = 0; l <= 10; l++)
+for (l = 0; l < 10; l++)
 {
 for (k = 'a'; k <= 'z'; k++)
 {
