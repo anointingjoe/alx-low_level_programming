739448bdb65bf5a70259241dd6bd60774a7b831d
anointingjoe <josephodey.88@gmail.com>
2022-07-09 08:02:21 -0700

    leeap year

---
+++ 0x03-debugging/3-main_a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/** main - year
+ *Return: 0
+ */
+
+int main(void)
+{
+
+//02/29/2000
+int day = convert_day(02, 29);
+print_remaining_days(2,day,2000);
+returm (0)
+}
+
---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/** main - year
+ *Return: 0
+ */
+
+int main(void)
+{
+
+//04/01/1997
+int day = convert_day(04, 01);
+print_remaining_days(4,day,1997);
+returm (0)
+}
