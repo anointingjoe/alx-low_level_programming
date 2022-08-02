47a34b5211599671b433766864d734c93085b124
anointingjoe <josephodey.88@gmail.com>
2022-08-02 01:53:11 -0700

    multiple main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,30 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- *main - assign a random number to the variable n each time it is executed
- *and prints out based on condition
+ * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+int positive_or_negative(void)
+{
+	int n;
 
-int main(void)
-{
-int n;
-srand(time(0));
-n = i
-if (n > 0)
-{
-printf("%d is %s\n", n, "positive");
-}
-else if (n < 0)
-{
-printf("%d is %s\n", n, "negative");
-}
-else
-{
-printf("%d is %s\n", n, "zero");
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
 }
